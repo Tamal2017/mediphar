624d3090d8f77b5d9ed954595cce392f6811df18
Tamal2017 <tingangt@yahoo.fr>
2020-07-04 18:58:50 +0000

    :data: Initial commit

---
+++ src/main/java/com/mediphar/medipharengine/MedipharApplication.java
@@ -0,0 +1,13 @@
+package com.mediphar.medipharengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedipharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedipharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediphar/medipharengine/MedipharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediphar.medipharengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedipharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
